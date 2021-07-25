4f0bd8250785af7bb2ae4b19c840d7d43270b7b1
Lazytangent <pm123633@gmail.com>
2021-07-25 13:53:34 -0500

    Add comments

--- scripts/mac.sh
+++ scripts/mac.sh
@@ -4,10 +4,12 @@
 
 set -e
 
+# Set ~/Screenshots to be default folder to store screenshots
 cd ~
 mkdir -p ~/Screenshots
 defaults write com.apple.screencapture location "/Users/$(whoami)/Screenshots"
 killall SystemUIServer
 
+# Change some of Finder's defaults
 defaults write com.apple.finder ShowPathbar -bool true
 defaulst write com.apple.finder ShowStatusBar -bool true
